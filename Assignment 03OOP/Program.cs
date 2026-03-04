fecaeee237fa9296c0aab2919d3ae2c616e9cf38
Basmala Ramadan <basmalaramadan51@gmail.com>
2026-03-05 01:39:40 +0200

    Add project files.

---
+++ Assignment 03OOP/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment_03OOP
+{
+    #region Q01
+  #endregion
+
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
