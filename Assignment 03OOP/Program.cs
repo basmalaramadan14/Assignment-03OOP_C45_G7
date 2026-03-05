595f8df69c046cd7d9b077dc9de2fe4b9eefcea1
Basmala Ramadan <basmalaramadan51@gmail.com>
2026-03-05 02:38:04 +0200

    Q02 part01

--- Assignment 03OOP/Program.cs
+++ Assignment 03OOP/Program.cs
@@ -18,7 +18,28 @@
     //--Dependency
 
     #endregion
+    #region  Q02 
+    //a) A parent class has a protected field.Can a child class in a different assembly access it?
+    //--Yes
+    //What about through an object instance from outside?
+    //--No
 
+    //b) What is the difference between protected internal and private protected?
+    //--protected internal — Accessible anywhere in the same assembly (like internal) OR in derived classes in other assemblies (like protected)
+    //--private protected — Accessible in the containing class OR derived classes in the same assembly only
+
+    //c) What does the sealed keyword do when applied to a class?
+    //-- Prevents inheritance
+    //What about when applied to a method?
+    //--Prevents further overriding
+
+
+    //d) Can you create an object from a sealed class using new? Why or why not?
+    //--Yes you can create an object from a sealed class using new.
+    //Because sealed only prevents inheritance, not object creation.
+    
+
+    #endregion
 
     internal class Program
     {
