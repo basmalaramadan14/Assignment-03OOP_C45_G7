833e61376c7e4fc346f4be66c217aad8c9031109
Basmala Ramadan <basmalaramadan51@gmail.com>
2026-03-05 02:08:45 +0200

    Q01 part01

--- Assignment 03OOP/Program.cs
+++ Assignment 03OOP/Program.cs
@@ -1,9 +1,25 @@
 ﻿namespace Assignment_03OOP
 {
     #region Q01
-  #endregion
+    //a) A University has Departments.If the university is closed, the departments no longer exist.
+    //--Composition
 
-   
+    //b) A Driver uses a Car.The driver does not own the car.
+    //--Association
+
+    //c) A Dog is an Animal.
+    //
+    //--Inheritance 
+
+    //d) A Team has Players.If the team is deleted, the players still exist.
+    //--Aggregation
+
+    //e) A method receives a Logger as a parameter and calls it inside the method only.
+    //--Dependency
+
+    #endregion
+
+
     internal class Program
     {
         static void Main(string[] args)
